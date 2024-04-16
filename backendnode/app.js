c3bc6dd074e30a6dfcac6ebda1d949ab4aa2b6df
oku <oukokus@gmail.com>
2024-04-16 15:28:25 +0900

    カレンダー作成前

--- backendnode/app.js
+++ backendnode/app.js
@@ -8,44 +8,45 @@ app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const mysql = require("mysql2");
-const cors = require('cors');
-
+const cors = require("cors");
 
 const con = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
   password: "rootroot",
   database: "world",
-  dateStrings: 'date' 
+  dateStrings: "date",
 });
 
 app.use(cors());
 app.get("/api/get/category", (req, res) => {
-     const sqlSelect = "SELECT * FROM sample3";
-     con.query(sqlSelect, (err, result) => {
-         res.send(result);
-     });
+  const sqlSelect = "SELECT * FROM sample3";
+  con.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
 });
 app.get("/api/get/info", (req, res) => {
   res.send("This is the GET handler for /api/get/info endpoint");
 });
-  
+
 app.post("/api/info", (req, res) => {
   const data = req.body;
-  console.log(data)
-  const columns = Object.keys(data).join(', '); // データオブジェクトから列名を抽出してカンマで連結します
-const values = Object.values(data).map(value => `'${value}'`).join(', '); // データオブジェクトから値を抽出してクオートで囲んでカンマで連結します
-const sql = `INSERT INTO sample3 (${columns}) VALUES (${values})`;
+  console.log(data);
+  const columns = Object.keys(data).join(", "); // データオブジェクトから列名を抽出してカンマで連結します
+  const values = Object.values(data)
+    .map((value) => `'${value}'`)
+    .join(", "); // データオブジェクトから値を抽出してクオートで囲んでカンマで連結します
+  const sql = `INSERT INTO sample3 (${columns}) VALUES (${values})`;
 
-console.log('Columns:', columns);
-console.log('Values:', values);
+  console.log("Columns:", columns);
+  console.log("Values:", values);
   con.query(sql, data, (error, results, fields) => {
     if (error) {
-      console.error('Error saving data:', error);
-      res.status(500).send('Error saving data');
+      console.error("Error saving data:", error);
+      res.status(500).send("Error saving data");
     } else {
-      console.log('Data saved successfully:', results);
-      res.status(200).send('Data received successfully!');
+      console.log("Data saved successfully:", results);
+      res.status(200).send("Data received successfully!");
     }
   });
 });
@@ -56,10 +57,9 @@ app.get("/", (req, res) => {
   app.use(express.static("assets"));
   const sql = "select * from sample3";
 
-  
   // ==========ここまでの範囲で書くようにしましょう。==========
   app.post("/", (req, res) => {
-    const sql = 'INSERT INTO sample3 SET ?'
+    const sql = "INSERT INTO sample3 SET ?";
     con.query(sql, req.body, function (err, result, fields) {
       if (err) throw err;
       console.log(result);
@@ -76,11 +76,6 @@ app.get("/", (req, res) => {
   });
 
   app.use(express.json());
-
-
-
- 
 });
-  
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
